4e7c605058a0f4bf93de7e4c559cd36068cf1305
Minsun <minisundev@gmail.com>
2024-03-14 14:03:52 +0900

    feat: init commit

---
+++ src/main/java/com/goohwang/woroutine/WoroutineApplication.java
@@ -0,0 +1,13 @@
+package com.goohwang.woroutine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoroutineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoroutineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goohwang/woroutine/WoroutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goohwang.woroutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoroutineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
