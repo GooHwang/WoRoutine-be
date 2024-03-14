27d44ec07c6f8289a1128cfd1d4a2d657c570f7e
Minsun <minisundev@gmail.com>
2024-03-14 19:21:44 +0900

    feat: Implement sign-up method in UserService #2

--- src/main/java/com/goohwang/woroutine/domain/user/service/UserService.java
+++ src/main/java/com/goohwang/woroutine/domain/user/service/UserService.java
@@ -1,6 +1,10 @@
 package com.goohwang.woroutine.domain.user.service;
 
+import com.goohwang.woroutine.domain.user.dto.request.SignUpRequest;
+import com.goohwang.woroutine.domain.user.entity.User;
 import com.goohwang.woroutine.domain.user.repository.UserRepository;
+import com.goohwang.woroutine.global.exception.ErrorCode;
+import com.goohwang.woroutine.global.exception.GlobalException;
 import com.goohwang.woroutine.global.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,4 +17,16 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
+
+    public void signup(SignUpRequest request) {
+        String email = request.email();
+        String password = passwordEncoder.encode(request.password());
+
+        if (userRepository.findByEmail(email).isPresent()) {
+            throw new GlobalException(ErrorCode.USER_ALREADY_EXIST);
+        }
+
+        User user = new User(email, password, request.nickname());
+        userRepository.save(user);
+    }
 }
